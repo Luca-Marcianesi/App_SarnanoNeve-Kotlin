2d103c5125de3b75c48c4cb2f7e139199c9bf65a
luca1 <s1093754@studenti.univpm.it>
2022-05-11 16:39:44 +0200

    aggiustamento warning

---
+++ app/src/main/java/com/example/myapplication/schermataOpzioni/InfoClass.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication.schermataOpzioni
+
+data class InfoClass (var image : Int , var testo : String)
---
+++ app/src/main/java/com/example/myapplication/schermataOpzioni/ListAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication.schermataOpzioni
+
+import android.app.Activity
+import com.example.myapplication.R
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class ListAdapter(val context: Activity, val list: ArrayList<InfoClass>) :
+    ArrayAdapter<InfoClass>(context, R.layout.layout_item_info, list) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+
+        val view: View = inflater.inflate(R.layout.layout_item_info, null)
+
+        val logo = view.findViewById<ImageView>(R.id.simbolo)
+        val testo = view.findViewById<TextView>(R.id.text_info)
+
+        logo.setImageResource(list[position].image)
+        testo.text = list[position].testo
+
+        return view
+    }
+
+
+}
+
+
+
+
+
+
+
