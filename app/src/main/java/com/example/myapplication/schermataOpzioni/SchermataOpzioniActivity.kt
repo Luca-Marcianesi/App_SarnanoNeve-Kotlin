88bf0629e0302428499cf59dfe6040bbb0684e62
luca1 <s1093754@studenti.univpm.it>
2022-05-11 16:39:23 +0200

    aggiustamento warning

--- app/src/main/java/com/example/myapplication/schermataOpzioni/SchermataOpzioniActivity.kt
+++ app/src/main/java/com/example/myapplication/schermataOpzioni/SchermataOpzioniActivity.kt
@@ -6,18 +6,39 @@ import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 
+lateinit var arrayInfo: ArrayList<InfoClass>
+
 class SchermataOpzioniActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_schermata_opzioni)
         val lista = findViewById<ListView>(R.id.listView)
-        val array = ArrayList<String>()
-        array.add("NOLEGGIO ATTREZZATURA")
-        array.add("INFORMAZIONI")
-        array.add("SCUOLA SCII")
-        array.add("PARCHEGGIO")
-        array.add("SOCCORSO PISTE")
-        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,array)
-        lista.adapter = adapter
+        val arrayImmagini = intArrayOf(
+            R.drawable.ic_baseline_calendar_today_24,
+            R.drawable.ic_baseline_calendar_today_24,
+            R.drawable.ic_baseline_calendar_today_24,
+            R.drawable.ic_baseline_calendar_today_24,
+            R.drawable.ic_baseline_calendar_today_24,
+            R.drawable.ic_baseline_calendar_today_24,
+        )
+
+        val arrayTesto = arrayOf(
+            getString(R.string.Impianti),
+            getString(R.string.Impianti),
+            getString(R.string.Impianti),
+            getString(R.string.Impianti),
+            getString(R.string.Impianti),
+            getString(R.string.Impianti)
+        )
+
+        arrayInfo = ArrayList()
+
+        for (i in arrayImmagini.indices) {
+            val info = InfoClass(arrayImmagini[i], arrayTesto[i])
+            arrayInfo.add(info)
+        }
+        lista.isClickable = true
+        lista.adapter = ListAdapter(this, arrayInfo)
+
     }
 }
