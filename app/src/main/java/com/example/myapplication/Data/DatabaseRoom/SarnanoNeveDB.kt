295630c5278320702dae21e184e16c822add0a66
luca1 <s1093754@studenti.univpm.it>
2022-07-11 18:09:02 +0200

    grandi modifiche

--- app/src/main/java/com/example/myapplication/Data/DatabaseRoom/SarnanoNeveDB.kt
+++ app/src/main/java/com/example/myapplication/Data/DatabaseRoom/SarnanoNeveDB.kt
@@ -4,12 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.myapplication.Data.DatabaseRoom.Dao.ImpiantoDao
 import com.example.myapplication.Data.DatabaseRoom.Dao.PistaDao
-import com.example.myapplication.Data.DatabaseRoom.Dao.SkipassDao
-import com.example.myapplication.Data.DatabaseRoom.Entity.Impianto
 import com.example.myapplication.Data.DatabaseRoom.Entity.Pista
-import com.example.myapplication.Data.DatabaseRoom.Entity.Skipass
 
 
 @Database(entities = [Pista::class], version=1)
