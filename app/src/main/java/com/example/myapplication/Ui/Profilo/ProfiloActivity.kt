deb79d3b34dcfdfb52bd66bb56d2dee3876912f1
luca1 <s1093754@studenti.univpm.it>
2022-07-19 18:11:27 +0200

    salvataggio account

--- app/src/main/java/com/example/myapplication/Ui/Profilo/ProfiloActivity.kt
+++ app/src/main/java/com/example/myapplication/Ui/Profilo/ProfiloActivity.kt
@@ -3,9 +3,12 @@ package com.example.myapplication.Ui.Profilo
 import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
+import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.core.text.set
 import androidx.lifecycle.ViewModelProviders
 import com.example.myapplication.Data.DatabaseRoom.Entity.Account
 import com.example.myapplication.R
@@ -53,8 +56,7 @@ class ProfiloActivity : ScopeActivity(), KodeinAware {
 
         datapicker = findViewById<EditText>(R.id.compleanno)
         editNome = findViewById<EditText>(R.id.editNome)
-        editCognome = findViewById<EditText>(R.id.editNome)
-        editData = findViewById<EditText>(R.id.editNome)
+        editCognome = findViewById<EditText>(R.id.editCognome)
         listPiste = findViewById(R.id.listaPiste)
         calendar = Calendar.getInstance()
 
@@ -79,8 +81,13 @@ class ProfiloActivity : ScopeActivity(), KodeinAware {
         }
 
         datapicker.setOnClickListener {
-                DatePickerDialog(this@ProfiloActivity, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)).show()
+            DatePickerDialog(
+                this@ProfiloActivity,
+                dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
 
 
@@ -91,8 +98,8 @@ class ProfiloActivity : ScopeActivity(), KodeinAware {
     }
 
 
-    private fun setUpList()  = launch {
-       val piste =  viewModel.pistePreferite.await()
+    private fun setUpList() = launch {
+        val piste = viewModel.pistePreferite.await()
         piste.observe(this@ProfiloActivity) { listaPiste ->
             listPiste.adapter = ListAdapterPiste(
                 this@ProfiloActivity, listaPiste, viewModel.pisteDao
@@ -102,38 +109,61 @@ class ProfiloActivity : ScopeActivity(), KodeinAware {
     }
 
 
-
-
-private fun setUpCampi() {
-    val account = viewModel.getAccount()
-    if (account != null) {
-        editNome.hint = account.nome
-        editCognome.hint = account.cognome
+    private fun setUpCampi() {
+        val account = viewModel.getAccount()
+        if (account != null) {
+            editNome.hint = account.nome
+            editCognome.hint = account.cognome
+            datapicker.hint = account.data_nascita
+        }
+        editNome.isEnabled = false
+        editCognome.isEnabled = false
+        datapicker.isEnabled = false
     }
 
-}
+    private fun setUpButtom() {
+        bottone_salva.setOnClickListener {
+            bottone_salva.visibility = View.GONE
+            bottone_modifica.visibility = View.VISIBLE
 
-private fun setUpButtom() {
-    bottone_salva.setOnClickListener {
-        bottone_salva.visibility = View.GONE
-        bottone_modifica.visibility = View.VISIBLE
-        viewModel.insertAccount(
-            Account(
-                nome = editNome.text.toString(),
-                cognome = editCognome.text.toString(),
-                data_nascita = editData.text.toString()
+            val account = viewModel.getAccount()
+
+            val nome =
+                if (editNome.text.toString() != "" || account != null) editNome.text.toString()
+                else account.nome
+            val cognome =
+                if (editCognome.text.toString() != "" || account != null) editNome.text.toString()
+                else account.cognome
+            val data =
+                if (datapicker.text.toString() != "" || account != null) editNome.text.toString()
+                else account.data_nascita
+
+            viewModel.insertAccount(
+                Account(
+                    nome = nome,
+                    cognome = cognome,
+                    data_nascita = data
+                )
             )
-        )
+
+            editNome.isEnabled = false
+            editCognome.isEnabled = false
+            datapicker.isEnabled = false
+
+            Toast.makeText(applicationContext,"Account salvato",1000)
+        }
+        bottone_modifica.setOnClickListener {
+            bottone_modifica.visibility = View.GONE
+            bottone_salva.visibility = View.VISIBLE
+            editNome.isEnabled = true
+            editCognome.isEnabled = true
+            datapicker.isEnabled = true
+            editNome.requestFocus()
+            val imm =
+                editNome.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+        }
     }
-    bottone_modifica.setOnClickListener {
-        bottone_modifica.visibility = View.GONE
-        bottone_salva.visibility = View.VISIBLE
-        editNome.requestFocus()
-        val imm =
-            editNome.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-    }
-}
 }
 
 
